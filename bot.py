f4e760749cf2bd0b5a730948e03afb9b278ede25
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-06-14 18:42:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,28 +37,5 @@ def _telegram_file(client, message):
   subprocess.call(['sudo','rm','-r',"./downloads/entry"]) 
  
  
-@bot.on_message(filters.private & filters.incoming & filters.voice )
-
-def _telegram_file(client, message):
-  try: 
-    with open("./output", 'r') as fh:
-        
-            sent_message = message.reply_text('هناك عملية  تتم الآن . أرسل الصوتية بعد مدة من فضلك', quote=True)
-            return
-  except FileNotFoundError: 
-    pass  
-  user_id = message.from_user.id
-  sent_message = message.reply_text('جار الفصل \n\n قال رسول الله ﷺ  لَيَكونَنَّ مِن أُمَّتي أقْوامٌ يَسْتَحِلُّونَ الحِرَ والحَرِيرَ، والخَمْرَ والمَعازِفَ، ولَيَنْزِلَنَّ أقْوامٌ إلى جَنْبِ عَلَمٍ، يَرُوحُ عليهم بسارِحَةٍ لهمْ، يَأْتِيهِمْ -يَعْنِي الفقِيرَ- لِحاجَةٍ، فيَقولونَ: ارْجِعْ إلَيْنا غَدًا، فيُبَيِّتُهُمُ اللَّهُ، ويَضَعُ العَلَمَ، ويَمْسَخُ آخَرِينَ قِرَدَةً وخَنازِيرَ إلى يَومِ القِيامَةِ. ( صحيح البخاري)', quote=True)
-  file = message.voice
-  file_path = message.download(file_name="entry")
-
-    # Execute speech.py script with entry file
-  subprocess.call(['spleeter', 'separate', '-p', 'spleeter:2stems', '-o', 'output' , "./downloads/entry" ])
-  subprocess.call(['mv',"./output/entry/vocals.wav" , "./output/entry/vocals.mp3" ])
-    # Upload transcription file to user
-  with open('./output/entry/vocals.mp3', 'rb') as f:
-        bot.send_audio(message.chat.id, f)
-  subprocess.call(['sudo','rm','-r',"output"])  
-   
 
 bot.run()
