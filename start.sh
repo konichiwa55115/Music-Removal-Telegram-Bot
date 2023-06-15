546512853909e81f9c1eac6bf99bf7cff87e5730
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-06-15 07:38:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ echo "Cloning Repo...."
 git clone https://github.com/konichiwa55115/transtest /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
+pip3 install spleeter
 echo "Starting Bot...."
 python3 bot.py
