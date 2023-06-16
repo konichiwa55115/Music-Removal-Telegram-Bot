1ce794c78496191a1c017089274e70e9f772c753
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-06-16 22:46:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = Client(
     "myfirs",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="5714654934:AAEVIR8baWhJcgUOtWeNmrSjvdRfYRiY7tI"
+    bot_token="6199159516:AAEl9HnYYOgpOI_LMRibWarruVQMiRGNkhU"
 )
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
