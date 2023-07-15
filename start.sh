b39a817accecd471127a2509bf07311eb776cebe
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-15 07:54:56 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/konichiwa55115/Music-Removal-Telegram-Bot /kony
 cd /kony
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-gunicorn app:app & python3 bot.py
+python3 bot.py
